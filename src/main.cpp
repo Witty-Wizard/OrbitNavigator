18863b5cb4f100d678b0d68885626585ce6dd560
Witty-Wizard <agarwalshashank429+github@gmail.com>
2024-12-03 11:40:23 +0530

    Added: Code for BNO085 IMU

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,112 @@
 #include "common/mavlink.h"
+#include <Adafruit_BNO08x.h>
 #include <Arduino.h>
 
-#define SERIAL_BAUD 115200       // MAVLink communication baud rate
-#define SYSTEM_ID 1              // MAVLink system ID (unique identifier for the ESP32)
-#define COMPONENT_ID 1           // MAVLink component ID
-#define MAV_TYPE MAV_TYPE_ROCKET // MAVType
+#define SERIAL_BAUD 115200 ///< MAVLink communication baud rate
+#define SYSTEM_ID 1    ///< MAVLink system ID (unique identifier for the ESP32)
+#define COMPONENT_ID 1 ///< MAVLink component ID
+#define MAV_TYPE MAV_TYPE_ROCKET ///< MAVType
+
+#define BNO08X_RESET -1
+
+struct euler_t {
+  float yaw;
+  float pitch;
+  float roll;
+} ypr;
 
 mavlink_message_t msg;
 
-void setup()
-{
-  // Initialize serial communication
-  Serial.begin(SERIAL_BAUD); // MAVLink communication
+Adafruit_BNO08x bno08x(BNO08X_RESET);
+sh2_SensorValue_t sensorValue;
+
+#ifdef FAST_MODE
+// Top frequency is reported to be 1000Hz (but freq is somewhat variable)
+sh2_SensorId_t reportType = SH2_GYRO_INTEGRATED_RV;
+long reportIntervalUs = 2000;
+#else
+// Top frequency is about 250Hz but this report is more accurate
+sh2_SensorId_t reportType = SH2_ARVR_STABILIZED_RV;
+long reportIntervalUs = 5000;
+#endif
+
+void setReports(sh2_SensorId_t reportType, long report_interval) {
+  Serial.println("Setting desired reports");
+  if (!bno08x.enableReport(reportType, report_interval)) {
+    Serial.println("Could not enable stabilized remote vector");
+  }
 }
 
-void loop()
-{
-  // Define a heartbeat message
+void setup() {
+  Serial.begin(SERIAL_BAUD);
+  Wire.setPins(18, 5);
+  if (!bno08x.begin_I2C()) {
+    Serial.println("Failed to find BNO08x chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("BNO08x Found!");
+  setReports(reportType, reportIntervalUs);
+
+  Serial.println("Reading events");
+  delay(100);
+}
+
+void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t *ypr,
+                       bool degrees = false) {
+
+  float sqr = sq(qr);
+  float sqi = sq(qi);
+  float sqj = sq(qj);
+  float sqk = sq(qk);
+
+  ypr->yaw = atan2(2.0 * (qi * qj + qk * qr), (sqi - sqj - sqk + sqr));
+  ypr->pitch = asin(-2.0 * (qi * qk - qj * qr) / (sqi + sqj + sqk + sqr));
+  ypr->roll = atan2(2.0 * (qj * qk + qi * qr), (-sqi - sqj + sqk + sqr));
+
+  if (degrees) {
+    ypr->yaw *= RAD_TO_DEG;
+    ypr->pitch *= RAD_TO_DEG;
+    ypr->roll *= RAD_TO_DEG;
+  }
+}
+
+void quaternionToEulerRV(sh2_RotationVectorWAcc_t *rotational_vector,
+                         euler_t *ypr, bool degrees = false) {
+  quaternionToEuler(rotational_vector->real, rotational_vector->i,
+                    rotational_vector->j, rotational_vector->k, ypr, degrees);
+}
+
+void quaternionToEulerGI(sh2_GyroIntegratedRV_t *rotational_vector,
+                         euler_t *ypr, bool degrees = false) {
+  quaternionToEuler(rotational_vector->real, rotational_vector->i,
+                    rotational_vector->j, rotational_vector->k, ypr, degrees);
+}
+
+void loop() {
   mavlink_message_t msg;
   uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
 
-  // Pack the heartbeat message
-  mavlink_msg_heartbeat_pack(
-      SYSTEM_ID,             // System ID
-      COMPONENT_ID,          // Component ID
-      &msg,                  // MAVLink message
-      MAV_TYPE,              // Type of MAV
-      MAV_AUTOPILOT_GENERIC, // Autopilot type
-      MAV_MODE_PREFLIGHT,    // MAVLink mode (set to preflight)
-      0,                     // Custom mode (not used here)
-      MAV_STATE_STANDBY      // MAVLink state
-  );
+  if (bno08x.wasReset()) {
+    Serial.print("sensor was reset ");
+    setReports(reportType, reportIntervalUs);
+  }
 
-  // Serialize the MAVLink message to the buffer
-  uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
-
-  // Send the serialized message over serial
-  Serial.write(buffer, len);
-
-  // Send heartbeat every second
-  delay(1000);
+  if (bno08x.getSensorEvent(&sensorValue)) {
+    // in this demo only one report type will be received depending on FAST_MODE
+    // define (above)
+    switch (sensorValue.sensorId) {
+    case SH2_ARVR_STABILIZED_RV:
+      quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, true);
+    case SH2_GYRO_INTEGRATED_RV:
+      // faster (more noise?)
+      quaternionToEulerGI(&sensorValue.un.gyroIntegratedRV, &ypr, true);
+      break;
+    }
+    mavlink_msg_attitude_pack(SYSTEM_ID, COMPONENT_ID, &msg, millis(), ypr.roll,
+                              ypr.pitch, ypr.yaw, 0, 0, 0);
+    uint16_t len = mavlink_msg_to_send_buffer(buffer, &msg);
+    Serial.write(buffer, len);
+  }
 }
